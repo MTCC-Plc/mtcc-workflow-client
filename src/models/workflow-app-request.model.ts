5664e1fb4600ce89fe60aaf1a8d508fae70a0812
Azzam Rafeeq <TheGamer@users.noreply.github.com>
2025-06-15 14:45:51 +0100

    action taken by included

--- src/models/workflow-app-request.model.ts
+++ src/models/workflow-app-request.model.ts
@@ -1,6 +1,26 @@
+export class WorkflowUser {
+  id!: number;
+  fullName!: string;
+  userId!: string;
+  email!: string;
+  rcno?: string;
+}
+
+export class WorkflowRequestStep {
+  id!: number;
+  actionTakenDate?: string;
+  workflowStepId?: number;
+  workflowRequestId?: number;
+  remarks?: string;
+  state!: string;
+  actionTakenBy?: WorkflowUser;
+}
+
 export class WorkflowAppRequest {
   id!: string;
+  requestId!: string;
   isCompleted!: boolean;
   isReceived!: boolean;
-  requestId!: string;
+
+  workflowRequestSteps?: WorkflowRequestStep[]; // ✅ Add this
 }
